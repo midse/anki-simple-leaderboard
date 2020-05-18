00101e7ac12cdef07fb4255637a6dfc0babfe8d1
Dimitri SEGARD <dimitri.segard@gmail.com>
2020-05-18 12:45:09 +0900

    Add back skeleton

---
+++ back.py
@@ -0,0 +1,34 @@
+from flask import Flask, url_for
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/teams", methods=["GET", "POST"])
+def teams():
+    if request.method == "POST":
+        return ""
+
+
+@app.route("/teams/<uuid:team_id>", methods=["GET", "PUT"])
+def team(username):
+    return ""
+
+
+@app.route("/users", methods=["GET", "POST"])
+def users():
+    # id : uuid
+    # name
+    # daily_reviews
+    # minutes_reviews
+    # streak
+    # monthly_reviews
+    # retention
+    # total_cards
+    if request.method == "POST":
+        return ""
+
+
+@app.route("/users/<uuid:user_id>", methods=["GET", "PUT"])
+def user(username):
+    return ""
